5c63c14fe485d2303724070981481cfdebdd3ef4
Lautaro Mansilla <85461971+laumansillaa@users.noreply.github.com>
2022-08-19 11:53:01 -0300

    add smart-contract

---
+++ game/contracts/MyEpicGame.sol
@@ -0,0 +1,261 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+// NFT contract to inherit from
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// Helper functions OpenZeppelin provides
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "hardhat/console.sol";
+import "./libraries/Base64.sol";
+
+contract MyEpicGame is ERC721 {
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    struct BigBoss {
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    BigBoss public bigBoss;
+
+    //The tokenId is the NFTs unique identifier, its just a number thats goes
+    // 0, 1, 2 , 3 etc
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    CharacterAttributes[] defaultCharacters;
+
+    // We create a mapping from the nft's tokenId => that NFTs attributes
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+    // nftHolderAttributes will be where we store the state of the player's NFTs.
+    // We map the the NFT's id to a CharacterAttributes struct.
+
+    //A mapping from an address => the NFT tokenId. Gives me an ez way
+    //to store the owner of the NFT and reference it later;
+    mapping(address => uint256) public nftHolders;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg,
+        string memory bossName,
+        string memory bossImageURI,
+        uint256 bossHp,
+        uint256 bossAttackDamage
+    )
+        // This is the name and symbol for our token, ex Ethereum and ETH. I just call mine
+        ERC721("Heroes", "Hero")
+    {
+        bigBoss = BigBoss({
+            name: bossName,
+            imageURI: bossImageURI,
+            hp: bossHp,
+            maxHp: bossHp,
+            attackDamage: bossAttackDamage
+        });
+
+        console.log(
+            "Done initializing boss %s w/ HP %s, img %s",
+            bigBoss.name,
+            bigBoss.hp,
+            bigBoss.imageURI
+        );
+
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
+
+        _tokenIds.increment();
+    }
+
+    function attackBoss() public {
+        uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+        CharacterAttributes storage player = nftHolderAttributes[
+            nftTokenIdOfPlayer
+        ];
+        // Basically, when we do storage and then do player.hp = 0 then it would change the health value on the NFT itself to 0.
+        console.log(
+            "\nPlayer w/ character %s about to attack. Has %s HP and %s AD",
+            player.name,
+            player.hp,
+            player.attackDamage
+        );
+        console.log(
+            "Boss %s has %s HP and %s AD",
+            bigBoss.name,
+            bigBoss.hp,
+            bigBoss.attackDamage
+        );
+
+        require(player.hp > 0, "Error: character must have HP to attack boss");
+
+        require(bigBoss.hp > 0, "Error: boss must have HP to attack character");
+
+        if (bigBoss.hp < player.attackDamage) {
+            bigBoss.hp = 0;
+        } else {
+            bigBoss.hp = bigBoss.hp - player.attackDamage;
+        }
+
+        if (player.hp < bigBoss.attackDamage) {
+            player.hp = 0;
+        } else {
+            player.hp = player.hp - bigBoss.attackDamage;
+        }
+
+        emit AttackComplete(msg.sender, bigBoss.hp, player.hp);
+        console.log("PLayer attacked boss. New Boss hp: %s", bigBoss.hp);
+        console.log("Boss attacked player. New player hp: %s\n", player.hp);
+    }
+
+    function checkIfUserHasNFT()
+        public
+        view
+        returns (CharacterAttributes memory)
+    {
+        // Get the tokenId of the user's character NFT
+        // If the user has a tokenId in the map, return their character.
+        // Else, return an empty character.
+        uint256 userNftTokenId = nftHolders[msg.sender];
+        if (userNftTokenId > 0) {
+            return nftHolderAttributes[userNftTokenId];
+        } else {
+            CharacterAttributes memory emptyStruct;
+            return emptyStruct;
+        }
+        // Why do we do userNftTokenId > 0? Well, basically there's no way to check if a key in a map exists.
+        // We set up our map like this: mapping(address => uint256) public nftHolders.
+        // No matter what key we look for, there will be a default value of 0.
+        // This is a problem for user's with NFT tokenId of 0. That's why earlier, I did _tokenIds.increment() in the constructor!
+        // That way, no one is allowed to have tokenId 0.
+    }
+
+    function getAllDefaultCharacters()
+        public
+        view
+        returns (CharacterAttributes[] memory)
+    {
+        return defaultCharacters;
+    }
+
+    function getBigBoss() public view returns (BigBoss memory) {
+        return bigBoss;
+    }
+
+    event CharacterNFTMinted(
+        address sender,
+        uint256 tokenId,
+        uint256 characterIndex
+    );
+    event AttackComplete(
+        address sender,
+        uint256 newBossHp,
+        uint256 newPlayerHp
+    );
+
+    // Users would be able to hit this function and get their NFT based on the
+    // characterId they send in!
+    function minCharacterNFT(uint256 _characterIndex) external {
+        //Get current tokenId (starts at 1 since we incremented in the constructor)
+        uint256 newItemId = _tokenIds.current();
+
+        //The magical function! Assigns the tokenId to the caller's wallet address
+        _safeMint(msg.sender, newItemId);
+
+        //We map the tokenId => their character attributes. More on this in
+        // the leasson below.
+
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log(
+            "Minted NFT w/ tokenId and characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        //Keep an ease way to see who owns what NFT
+        nftHolders[msg.sender] = newItemId;
+
+        //Increment the tokenId for the next person that uses it
+        _tokenIds.increment();
+
+        emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDamage
+        );
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                charAttributes.name,
+                " -- NFT #: ",
+                Strings.toString(_tokenId),
+                '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+                charAttributes.imageURI,
+                '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                strHp,
+                ', "max_value":',
+                strMaxHp,
+                '}, { "trait_type": "Attack Damage", "value": ',
+                strAttackDamage,
+                "} ]}"
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        return output;
+    }
+}
---
+++ game/scripts/deploy.js
@@ -0,0 +1,47 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy(
+        ["Miguel One", "Abigail Lincoln", "Wallabee Beatles"],
+        ["https://pm1.narvii.com/6395/a6d4c2fcc6da652b2cecb99accbf932a2dd6e21e_hq.jpg", // Images
+        "https://static.wikia.nocookie.net/kidsnextdoorfanfiction/images/8/84/Abby_Lincoln.jpg/revision/latest/scale-to-width-down/400?cb=20140309003436", 
+        "https://pm1.narvii.com/6056/208cd1672451b895dc33c286a157f5c4e22e3ceb_hq.jpg"],    
+        [100, 200, 300],
+        [100, 50, 25],
+        "Mr. Boss",
+        "https://pm1.narvii.com/6225/dc8b7196b5569a201f3f5b714f8a6d2fe59d188f_hq.jpg",
+        10000, //BossHp,
+        50 //Boss Attack Damage 
+
+        );
+    // console.log('GAME COTNRACT', gameContract)
+    await gameContract.deployed();
+    console.log('Contract deployed to: ', gameContract.address);
+
+    //let txn;
+    ///we only have three characters
+    ///an nft w/ the character at index 2 of out array
+    // txn = await gameContract.minCharacterNFT(2);
+    // await txn.wait();
+
+    // txn = await gameContract.attackBoss();
+    // await txn.wait();
+
+    // txn = await gameContract.attackBoss();
+    // await txn.wait();
+
+    //get the value of the NFT uri´s
+    // let returnedTokenUri = await gameContract.tokenURI(1);
+    // console.log('Token URI', returnedTokenUri);
+};
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
